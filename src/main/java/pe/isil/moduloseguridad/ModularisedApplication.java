3bd56883649919c105767fef98ce2d4f7c3edd6b
Christian Valdivia <cva.250199@gmail.com>
2022-09-17 09:12:42 -0500

    springboo clase

---
+++ src/main/java/pe/isil/moduloseguridad/ModularisedApplication.java
@@ -0,0 +1,15 @@
+package pe.isil.moduloseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularisedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModularisedApplication.class, args);
+		System.out.println("hole undo");
+	}
+
+
+}
